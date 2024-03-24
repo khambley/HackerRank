7179757a9a029b54a9ee5dc4c84f75444be1fdea
Katherine Hambley <superdesigngirl@mac.com>
2024-03-24 12:41:41 -0500

    Added new folder for review 2024

---
+++ CSharp/NotesStore/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace NotesStore;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var notesStoreObj = new NotesStore();
+        var n = int.Parse(Console.ReadLine());
+        for (var i = 0; i < n; i++)
+        {
+            var operationInfo = Console.ReadLine().Split(' ');
+            try
+            {
+                if (operationInfo[0] == "AddNote")
+                    notesStoreObj.AddNote(operationInfo[1], operationInfo.Length == 2 ? "" : operationInfo[2]);
+                else if (operationInfo[0] == "GetNotes")
+                {
+                    var result = notesStoreObj.GetNotes(operationInfo[1]);
+                    if (result.Count == 0)
+                        Console.WriteLine("No Notes");
+                    else
+                        Console.WriteLine(string.Join(",", result));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Parameter");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
+
+class NotesStore
+{
+    public List<Note>? Notes { get; set; }
+
+    public void AddNote(string state, string name)
+    {
+        if (state == "active")
+        {
+            Notes = new List<Note>();
+
+            if (name != string.Empty)
+            {
+                var note = new Note { Name = name, State = state };
+                if (note != null)
+                {
+                    Notes.Add(note);
+                }
+
+            }
+            else
+            {
+                throw new ArgumentException("Name cannot be empty");
+            }
+        }
+        else
+        {
+            throw new ArgumentException("Invalid state: ", state);
+        }
+        
+    }
+
+    public List<string> GetNotes(string state)
+    {
+        var notes = new List<Note>();
+        var names = new List<string>();
+
+        if (state != "completed" || state != "active" || state != "others")
+            throw new ArgumentException("Invalid State ", state);
+
+        if (Notes != null)
+        {
+            notes = Notes.Where(n => n.State == state).ToList();
+
+            var name = "";
+
+            if(notes != null)
+            {
+                foreach (var note in notes)
+                {
+                    name = note.Name;
+
+                    if(name != null)
+                    {
+                        names.Add(name);
+                    }                   
+                }
+            }
+            else
+            {
+                return names;
+            }            
+        }        
+        return names;
+    }
+}
+
+public class Note
+{
+    public string? Name { get; set; }
+    public string? State { get; set; }
+}
+
---
+++ CSharp2024/PlusMinus/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace PlusMinus;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> array = [1,1,0,1,1];
+
+        plusMinus(array);
+    }
+    public static void plusMinus(List<int> arr)
+    {
+        var posCount = 0;
+        var negCount = 0;
+        var zeroCount = 0;
+        for(int i = 0; i < arr.Count; i++)
+        {
+            if ( arr[i] > 0 ){
+                posCount++;
+            }
+            else if ( arr[i] < 0 ){
+                negCount++;
+            }
+            else {
+                zeroCount++;
+            }
+        }
+        if(posCount > 0){
+                double posRatio = posCount / (double)arr.Count;
+                Console.WriteLine(posRatio.ToString("N6"));
+            }
+            else {
+                Console.WriteLine("0.000000");
+            }
+
+            if(negCount > 0){
+                var negRatio = negCount / (double)arr.Count;
+                Console.WriteLine(negRatio.ToString("N6"));
+            }
+            else {
+                Console.WriteLine("0.000000");
+            }
+
+            if(zeroCount > 0){
+                var zeroRatio = zeroCount / (double)arr.Count;
+                Console.WriteLine(zeroRatio.ToString("N6"));
+            }
+            else {
+                Console.WriteLine("0.000000");
+            }
+    }
+}
