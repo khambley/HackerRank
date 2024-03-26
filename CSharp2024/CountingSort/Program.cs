bc83977d9424a9c36feb3e5fda5de4ebe33c00d2
Katherine Hambley <superdesigngirl@mac.com>
2024-03-26 09:56:06 -0500

    Added CountingSort and Insert

---
+++ CSharp2024/CountingSort/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace CountingSort;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>();
+
+        Random random = new Random();
+        int randomNumber = 0;
+
+        for(int i = 0; i < 10; i++)
+        {
+            randomNumber = random.Next(1, 101);
+            numbers.Add(randomNumber);
+        }
+        
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+        Console.WriteLine();
+
+        countingSort(numbers);
+
+    }
+    public static List<int> countingSort(List<int> arr)
+    {
+        List<int> frequencyArray = new List<int>();
+
+
+        for (int i = 0; i <= 99; i++)
+        {
+            frequencyArray.Add(0);
+        }
+
+        int value = 0;
+        for (int i = 0; i < arr.Count; i++)
+        {
+            value = arr[i];
+            frequencyArray[value]++;
+        }
+        for (int i = 0; i < frequencyArray.Count; i++)
+        {
+            Console.Write(frequencyArray[i] + " ");
+        }
+        return frequencyArray;
+    }
+}
---
+++ CSharp2024/InsertIntoSortedList/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace InsertIntoSortedList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> array = new List<int>() {4,7,1,3,8,9,12,0,2};
+
+        var newItem = 5;
+
+        array.Sort();
+
+        var index = array.BinarySearch(newItem);
+
+        if (index < 0)
+        {
+            index = ~index;
+        }
+        array.Insert(index, newItem);
+
+        for (int i = 0; i < array.Count; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
